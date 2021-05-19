40efea09d5d2d08e1e1267ae0aee1095ab457450
gxgarciat <info@gxgarciat.dev>
2021-05-19 20:30:03 +0100

    Adding worked examples P2

---
+++ 4_Worked_Examples/2_Backflip.py
@@ -0,0 +1,20 @@
+from karel.stanfordkarel import *
+
+"""
+File: ForLoopWarmup.py
+------------------------------
+Get Karel to do a cool backflip by turning left 4 times.
+"""
+
+def main():
+    """
+    You should write your code to make Karel do its task in
+    this function. Make sure to delete the 'pass' line before
+    starting to write your own code. You should also delete this
+    comment and replace it with a better, more descriptive one.
+    """
+    for i in range(16):
+        turn_left()
+
+if __name__ == "__main__":
+    run_karel_program()
