15cfa94506f28a0d56df0e240a38415d0ed3e9a4
gxgarciat <info@gxgarciat.dev>
2021-05-26 16:16:27 +0100

    Worked example

---
+++ 4_Worked_Examples/2_UpstairsDownstairs.py
@@ -0,0 +1,41 @@
+from karel.stanfordkarel import *
+
+"""
+File: UpstairsDownstairs.py
+------------------------------
+Karel will climb three stair steps up and then three stair steps down.
+"""
+
+def main():
+    climbing()
+    descending()
+        
+
+
+def descending():
+    for i in range(3):
+        move()
+        turn_threetimes()
+        move()
+        turn_left()
+
+def climbing():
+    for i in range(3):
+        turn_left()
+        move()
+        turn_threetimes()
+        move()
+
+def turn_right():
+    for i in range(2):
+        turn_left()
+
+def turn_threetimes():
+    for i in range(3):
+        turn_left()
+
+
+    pass
+
+if __name__ == "__main__":
+    run_karel_program('UpstairsDownstairs.w')
