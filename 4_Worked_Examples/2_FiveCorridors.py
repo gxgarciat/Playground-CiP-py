62c6d796481a4da7f8508c7e6019c96d4f39b090
gxgarciat <info@gxgarciat.dev>
2021-05-26 16:15:51 +0100

    Worked example

---
+++ 4_Worked_Examples/2_FiveCorridors.py
@@ -0,0 +1,52 @@
+from karel.stanfordkarel import *
+
+"""
+File: FiveCorridorsKarel.py
+-----------------------
+Karel traverse 5 variable length corridors and place beepers at the ends of them if there aren't already beepers there.
+"""
+
+def main():
+    for i in range(4):
+        move_forward()
+        beeperstatus()
+        turn_right()
+        move_forward()
+        if front_is_blocked():
+            turn_threetimes()
+        move()
+        turn_threetimes()
+    move_forward()
+    beeperstatus()
+    turn_right()
+    move_forward()
+    turn_right()
+
+
+            
+    
+def move_forward():
+    while front_is_clear():
+        move()
+
+def beeperstatus():
+    if no_beepers_present():
+        put_beeper()
+    
+    
+def turn_right():
+    for i in range(2):
+        turn_left()
+
+def turn_threetimes():
+    for i in range(3):
+        turn_left()
+
+
+    
+    
+
+
+
+if __name__ == "__main__":
+    run_karel_program()
