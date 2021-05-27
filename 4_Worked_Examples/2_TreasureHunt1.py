6347b2d8bad33fdfa03dd32556280b389ca8e310
gxgarciat <info@gxgarciat.dev>
2021-05-27 16:53:16 +0100

    Worked Example

---
+++ 4_Worked_Examples/2_TreasureHunt1.py
@@ -0,0 +1,22 @@
+from karel.stanfordkarel import *
+
+"""
+File: TreasureHunt1.py
+------------------------------
+Karel will move forward until a beeper.
+At every beeper, Karel will turn left and move until the next beeper, until Karel is standing on the treasure, which is a pile of 10 beepers.
+"""
+
+def main():
+    while front_is_clear():
+        move()
+        if beepers_present():
+            while beepers_present():
+                pick_beeper()
+            turn_left()
+
+
+    pass
+
+if __name__ == "__main__":
+    run_karel_program('TreasureHunt1.w')
