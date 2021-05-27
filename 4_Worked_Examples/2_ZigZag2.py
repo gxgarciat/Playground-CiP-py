34c0196c820e402274c7c6315df19ff5945d26c9
gxgarciat <info@gxgarciat.dev>
2021-05-27 16:55:13 +0100

    Worked Example

---
+++ 4_Worked_Examples/2_ZigZag2.py
@@ -0,0 +1,38 @@
+from karel.stanfordkarel import *
+
+"""
+File: ZigZagKarel.py
+-----------------------
+Places a zig zag pattern of beepers in the world.
+"""
+
+def main():
+    while front_is_clear():
+        put_beeper()
+        top_beepers()
+        turn_right()
+        move()
+        turn_left()
+        if front_is_blocked():
+            pass
+        else: 
+            move()
+
+def top_beepers():
+    move()
+    turn_left()
+    move()
+    put_beeper()
+
+def turn_right():
+    for i in range(2):
+        turn_left()
+
+def turn_threetimes():
+    for i in range(3):
+        turn_left()
+
+    pass
+
+if __name__ == "__main__":
+    run_karel_program('ZigZag2.w')
