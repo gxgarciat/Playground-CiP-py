8b1012d4db45cb5449574b5157f076a168dadd49
gxgarciat <info@gxgarciat.dev>
2021-05-28 19:47:46 +0100

    Worked example

---
+++ 4_Worked_Examples/3_EMC2.py
@@ -0,0 +1,13 @@
+def main():
+    
+    C = 299792458
+    m = float(input("Enter kilos of mass: "))
+    print("e = m * C^2...")
+    print(f'm = {m} kg')
+    print(f'C = {C} m/s')
+    e=m*C**2 
+    print(f'{e} joules of energy!')
+    
+
+if __name__ == "__main__":
+    main()
