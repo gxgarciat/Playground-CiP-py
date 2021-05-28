eca986ad4c0f6c4dd119df1b5139750509a18eec
gxgarciat <info@gxgarciat.dev>
2021-05-28 19:50:29 +0100

    Worked example

---
+++ 4_Worked_Examples/3_SecondsInAYear.py
@@ -0,0 +1,11 @@
+def main():
+    
+    year = 365 
+    hours_day = 24 
+    min_hour = 60
+    sec_min = 60 
+    seconds_year= year*hours_day*min_hour*sec_min
+    print(f'There are {seconds_year} seconds in a year')
+
+if __name__ == "__main__":
+    main()
