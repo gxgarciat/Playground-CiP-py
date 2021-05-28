b614403083b3246172268d3b04c980298c7cff8d
gxgarciat <info@gxgarciat.dev>
2021-05-28 19:46:45 +0100

    Worked example

---
+++ 4_Worked_Examples/3_DogYears.py
@@ -0,0 +1,8 @@
+def main():
+    
+    age_human=float(input("Enter an age in human years: "))
+    age_dog=age_human*7.18
+    print(f"That's {age_dog} in dog years!")
+
+if __name__ == "__main__":
+    main()
