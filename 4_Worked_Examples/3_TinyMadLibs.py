37425bc55e78799ec03683befa28a62055c2a791
gxgarciat <info@gxgarciat.dev>
2021-05-28 19:52:56 +0100

    Worked example

---
+++ 4_Worked_Examples/3_TinyMadLibs.py
@@ -0,0 +1,10 @@
+SENTENCE_START = "Code in Place is fun. I learned to program and used Python to make my " # adjective noun verb
+
+def main():
+    adj=str(input("Please type an adjective and press enter. "))
+    noun=str(input("Please type an adjective and press enter. "))
+    verb=str(input("Please type an adjective and press enter. "))
+    print(f'Code in Place is fun. I learned to program and used Python to make my {adj} {noun} {verb}!')
+
+if __name__ == "__main__":
+    main()
