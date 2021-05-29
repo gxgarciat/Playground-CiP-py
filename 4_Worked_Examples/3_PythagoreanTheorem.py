9aef9887a2bd12c91bd4d2219ba48dd67615fbc2
gxgarciat <info@gxgarciat.dev>
2021-05-29 16:56:35 +0100

    Worked example

---
+++ 4_Worked_Examples/3_PythagoreanTheorem.py
@@ -0,0 +1,10 @@
+import math
+
+def main():
+    ab_length = float(input("Enter the length of AB: "))
+    ac_length = float(input("Enter the length of AC: "))
+    bc_hypo=(ab_length**2+ac_length**2)**(0.5)
+    print("The length of BC (the hypotenuse) is: ", bc_hypo)
+
+if __name__ == "__main__":
+    main()
