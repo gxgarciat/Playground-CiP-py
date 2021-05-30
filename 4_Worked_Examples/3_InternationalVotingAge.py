625d26fab39dd3bf71bddcc65fbe62a1ec04b4b7
gxgarciat <info@gxgarciat.dev>
2021-05-30 20:24:58 +0100

    Worked example

---
+++ 4_Worked_Examples/3_InternationalVotingAge.py
@@ -0,0 +1,26 @@
+def main():
+
+    age_peturksbouipo = 16
+    age_stanlau=25
+    age_mayengua=48
+
+    age=int(input("How old are you? "))
+    if age >= age_mayengua:
+        print("You can vote in Peturksbouipo where the voting age is 16.")
+        print("You can vote in Stanlau where the voting age is 25.")
+        print("You can vote in Mayengua where the voting age is 48.")
+    elif age >= age_stanlau:
+            print("You can vote in Peturksbouipo where the voting age is 16.")
+            print("You can vote in Stanlau where the voting age is 25.")
+            print("You cannot vote in Mayengua where the voting age is 48.")
+    elif age >= age_peturksbouipo:
+            print("You can vote in Peturksbouipo where the voting age is 16.")
+            print("You cannot vote in Stanlau where the voting age is 25.")
+            print("You cannot vote in Mayengua where the voting age is 48.")
+    else:
+            print("You cannot vote in Peturksbouipo where the voting age is 16.")
+            print("You cannot vote in Stanlau where the voting age is 25.")
+            print("You cannot vote in Mayengua where the voting age is 48.")
+
+if __name__ == "__main__":
+    main()
