7749b57fab5b308633de4c8877d18280a5c9ed9a
gxgarciat <info@gxgarciat.dev>
2021-05-30 01:22:26 +0100

    Worked example

---
+++ 4_Worked_Examples/3_Fibonacci.py
@@ -0,0 +1,21 @@
+def main():
+    
+    sum = 0
+    fib0= 0
+    fib1= 1
+    CONSTANT=10000
+    print(fib0)
+    print(fib1)
+    while 'TRUE':
+        sum  = fib1+fib0
+        fib0 = fib1
+        fib1 = sum
+        if (fib0 < CONSTANT) and (fib1 < CONSTANT):
+            print(sum)
+        else:
+            break
+
+        
+
+if __name__ == "__main__":
+    main()
