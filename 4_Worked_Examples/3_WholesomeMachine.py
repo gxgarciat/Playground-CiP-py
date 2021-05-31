e05c4e538e7ad14780caf5f6d1e46bf299c4dc19
gxgarciat <info@gxgarciat.dev>
2021-05-31 11:37:30 +0100

    Worked example

---
+++ 4_Worked_Examples/3_WholesomeMachine.py
@@ -0,0 +1,13 @@
+def main():
+
+
+    motivation="I am capable of doing anything I put my mind to."
+    text=input("Please type the following affirmation: I am capable of doing anything I put my mind to.")
+    while motivation != text:
+        print("That was not the affirmation.")
+        print("")
+        text=input("Please type the following affirmation: I am capable of doing anything I put my mind to.")
+    print("That's right! :)")
+
+if __name__ == "__main__":
+    main()
