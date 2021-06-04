aadb2b504a3bc431e6dd5ac811efb82461b87393
gxgarciat <info@gxgarciat.dev>
2021-06-04 17:45:16 +0100

    Assigment

---
+++ 1_Assigments/1_Q8_Extension.py
@@ -0,0 +1,27 @@
+from karel.stanfordkarel import *
+
+"""
+File: ExtensionKarel.py
+-----------------------
+This file is for optional extension programs. 
+"""
+
+def main():
+    """
+    You should write your code to make Karel do its task in
+    this function. Make sure to delete the 'pass' line before
+    starting to write your own code. You should also delete this
+    comment and replace it with a better, more descriptive one.
+    """
+    while front_is_clear():
+        move()
+        if beepers_present():
+            pick_beeper()
+    
+    if front_is_blocked():
+        put_beeper()
+
+
+
+if __name__ == "__main__":
+    run_karel_program()
