434a41914d5ba9f2af3e62fe7c07a5649a4dd3d0
gxgarciat <info@gxgarciat.dev>
2021-06-04 17:43:09 +0100

    Assigment

---
+++ 1_Assigments/1_Q7_Midpoint.py
@@ -0,0 +1,86 @@
+from karel.stanfordkarel import *
+
+"""
+File: Midpoint.py
+------------------------
+Place a beeper on the middle of the first row.
+"""
+def main():
+
+    while no_beepers_present():
+        build_wall()
+    if left_is_blocked():
+        turn_left()
+        back_to_start()
+        deconstruct_wall()
+        turn_left()
+        place_last_beeper()
+    else:
+        back_to_start()
+        while beepers_present():
+            deconstruct_wall()
+        place_last_beeper()
+
+def build_wall():
+    while no_beepers_present():
+        if front_is_clear():
+            put_beeper()
+            move()
+        else:
+            put_beeper()
+            turn_left()
+            if front_is_clear():
+                move()
+    if left_is_clear():
+        turn_left()
+        move()
+        turn_left()
+        move()
+        turn_right()
+
+def deconstruct_wall():
+    while beepers_present():
+        if front_is_clear():
+            pick_beeper()
+            move()
+        else:
+            pick_beeper()
+            turn_left()
+            if front_is_clear():
+                move()
+    if left_is_clear():
+        turn_left()
+        move()
+        turn_left()
+        move()
+        turn_right()
+
+def back_to_start():
+    turn_left()
+    while front_is_clear():
+        move()
+    turn_left()
+    while front_is_clear():
+        move()
+    turn_left()
+
+def place_last_beeper():
+    if facing_north():
+        turn_left()
+        turn_left()
+    if facing_east():
+        turn_right()
+    if facing_west():
+        turn_left()
+    while front_is_clear():
+        move()
+    put_beeper()
+    turn_right()
+
+def turn_right():
+    for i in range(3):
+        turn_left()
+    
+
+if __name__ == '__main__':
+    run_karel_program('Midpoint2.w')
