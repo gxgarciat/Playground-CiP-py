a992efaed3285e7f8db59ce031dd8a918d33dff8
gxgarciat <info@gxgarciat.dev>
2021-06-05 18:19:51 +0100

    Assigment

---
+++ 1_Assigments/2_Q3_SubstractNumbers.py
@@ -0,0 +1,14 @@
+"""
+Asks the user for two numbers and prints the result
+of the first number minus the second number.
+"""
+
+def main():
+    print("This program subtracts one number from another.")
+    first_number=float(input("Enter first number: "))
+    second_number=float(input("Enter second number: "))
+    result = first_number - second_number
+    print("The result is",result)
+
+if __name__ == '__main__':
+    main()
