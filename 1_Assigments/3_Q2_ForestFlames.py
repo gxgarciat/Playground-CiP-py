27a1654be26e1cb2a89d65125067cbc6aca67b29
gxgarciat <info@gxgarciat.dev>
2021-06-08 18:49:54 +0100

    Assigment

---
+++ 1_Assigments/3_Q2_ForestFlames.py
@@ -0,0 +1,110 @@
+"""
+This program highlights fires in an image by identifying pixels
+whose red intensity is more than INTENSITY_THRESHOLD times the
+average of the red, green, and blue values at a pixel. Those
+"sufficiently red" pixels are then highlighted in the image
+and other pixels are turned grey, by setting the pixel red,
+green, and blue values to be all the same average value.
+"""
+
+from simpleimage import SimpleImage
+
+INTENSITY_THRESHOLD = 1.0
+DEFAULT_FILE = 'images/greenland-fire.png'
+
+def find_flames(filename):
+    """
+    This function should highlight the "sufficiently red" pixels
+    in the image and grayscale all other pixels in the image
+    in order to highlight areas of wildfires.
+    """
+    image = SimpleImage(filename)
+    # TODO: your code here
+    return image
+
+def main():
+    # Get file and load image
+    filename = get_file()
+    image = SimpleImage(filename)
+
+    # Show the original fire
+    original_fire = SimpleImage(filename)
+    original_fire.show()
+
+    grayscale_flower = grayscale(filename)
+    grayscale_flower.show()
+
+
+    # Show the highlighted fire
+    highlighted_fire = find_flames(filename,grayscale_flower)
+    highlighted_fire.show()
+
+
+def get_file():
+    # Read image file path from user, or use the default file
+    filename = input('Enter image file (or press enter for default): ')
+    if filename == '':
+        filename = DEFAULT_FILE
+    return filename
+
+#-----------------------------------------------
+def darker(image):
+    """
+    Makes image passed in darker by halving red, green, blue values.
+    Note: changes in image persist after function ends.
+    """
+    # Demonstrate looping over all the pixels of an image,
+    # changing each pixel to be half its original intensity.
+    for pixel in image:
+        pixel.red = pixel.red // 2
+        pixel.green = pixel.green // 2
+        pixel.blue = pixel.blue // 2
+
+def compute_luminosity(red, green, blue):
+    """
+    Calculates the luminosity of a pixel using NTSC formula
+    to weight red, green, and blue values appropriately.
+    """
+    return (0.299 * red) + (0.587 * green) + (0.114 * blue)
+
+
+def grayscale(filename):
+    """
+    Reads image from file specified by filename.
+    Change the image to be grayscale using the NTSC
+    luminosity formula and return it.
+    """
+    image = SimpleImage(filename)
+    for pixel in image:
+        luminosity = compute_luminosity(pixel.red, pixel.green, pixel.blue)
+        pixel.red = luminosity
+        pixel.green = luminosity
+        pixel.blue = luminosity
+    return image
+
+def find_flames(main_filename, back_filename):
+    """
+    Implements the notion of "redscreening".  That is,
+    the image in the main_filename has its "sufficiently red"
+    pixels replaced with pixel from the corresponding x,y
+    location in the image in the file back_filename.
+    Returns the resulting "redscreened" image.
+    """
+    image = SimpleImage(main_filename)
+    back = SimpleImage(main_filename)
+    for pixel in image:
+        average = (pixel.red + pixel.green + pixel.blue) // 3
+        # See if this pixel is "sufficiently" red
+        if pixel.red >= average * INTENSITY_THRESHOLD:
+            # If so, we get the corresponding pixel from the
+            # back image and overwrite the pixel in
+            # the main image with that from the back image.
+            x = pixel.x
+            y = pixel.y
+            image.set_pixel(x, y, back.get_pixel(x, y))
+    return image
+
+
+
+if __name__ == '__main__':
+    main()
