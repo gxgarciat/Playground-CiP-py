930ca12e3764402923486c10298c94dd14b05ad8
gxgarciat <info@gxgarciat.dev>
2021-06-08 18:50:29 +0100

    Assigment

---
+++ 1_Assigments/3_Q3_Reflection.py
@@ -0,0 +1,39 @@
+"""
+This program takes an image and generates a reflection.
+The top half of the generated image is the same as the original.
+The bottom half is the mirror reflection of the top half.
+"""
+
+from simpleimage import SimpleImage
+
+DEFAULT_FILE = 'images/mt-rainier.jpg'
+
+def make_reflected(filename):
+    image = SimpleImage(filename)
+    # TODO: your code here.
+    return image
+
+
+def main():
+    # Get file and load image
+    filename = get_file()
+
+    # Show the original image
+    original = SimpleImage(filename)
+    original.show()
+
+    # Show the reflected image
+    reflected = make_reflected(filename)
+    reflected.show()
+
+
+def get_file():
+    # Read image file path from user, or use the default file
+    filename = input('Enter image file (or press enter for default): ')
+    if filename == '':
+        filename = DEFAULT_FILE
+    return filename
+
+
+if __name__ == '__main__':
+    main()
