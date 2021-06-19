fb4ab49cee249a79ccd803d1fa8024dc2e1dc629
gxgarciat <info@gxgarciat.dev>
2021-06-19 20:16:36 +0100

    Refactoring

--- 1_Assigments/1_Q4_CleanupKarelM2.py
+++ 1_Assigments/1_Q4_CleanupKarelM2.py
@@ -1,20 +1,12 @@
 from karel.stanfordkarel import *
 
-"""
-File: CleanupKarel.py
---------------------
-When you finish writing this file, CleanupKarel should be able to
-pick up all beepers from the first row of any sized world and
-end in the bottom right corner facing East.
-"""
-
 def main():
     """
-    You should write your code to make Karel do its task in
-    this function. Make sure to delete the 'pass' line before
-    starting to write your own code. You should also delete this
-    comment and replace it with a better, more descriptive one.
+    Write a code where CleanupKarel should be able to pick up all beepers 
+    from the first row of any sized world and end in the bottom right corner facing East.
     """
+
+# This code comprises a while-loop and a conditional, to check if beepers are present.
     while front_is_clear():
         move()
         if beepers_present():
