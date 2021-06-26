b2a9adfed3f46870b37415f2a62209d19008826f
gxgarciat <info@gxgarciat.dev>
2021-06-26 20:50:01 +0100

    Refactoring

--- 1_Assigments/2_Q3_SubstractNumbers.py
+++ 1_Assigments/2_Q3_SubstractNumbers.py
@@ -1,9 +1,10 @@
+def main():
+
 """
 Asks the user for two numbers and prints the result
 of the first number minus the second number.
 """
 
-def main():
     print("This program subtracts one number from another.")
     first_number=float(input("Enter first number: "))
     second_number=float(input("Enter second number: "))
