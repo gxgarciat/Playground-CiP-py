8c9695ab45f4673488fc47eac727bb8c8b010169
gxgarciat <info@gxgarciat.dev>
2021-07-12 21:57:12 +0100

    Refactoring

--- 1_Assigments/2_Q5_Liftoff.py
+++ 1_Assigments/2_Q5_Liftoff.py
@@ -1,8 +1,9 @@
-"""
-Prints out a spaceship launch sequence.
-"""
-
 def main():
+
+    """
+    Prints out a spaceship launch sequence.
+    """
+
     for i in range(10,0,-1):
         print(i)
     print("Liftoff!")
