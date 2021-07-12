51abd5687ae2f40645436af0deea840d239fbfeb
gxgarciat <info@gxgarciat.dev>
2021-07-12 21:56:22 +0100

    Refactoring

--- 1_Assigments/2_Q4_RandomNumbers.py
+++ 1_Assigments/2_Q4_RandomNumbers.py
@@ -1,5 +1,3 @@
-
-
 import random
 
 """
@@ -9,7 +7,11 @@ This function prints out 10 random numbers between 0 and 100.
 NUM_RANDOM=10
 MIN_RANDOM=0
 MAX_RANDOM=100
+
+
 def main():
+
+
     NUM_RANDOM=10
     for i in range(NUM_RANDOM):
         print(random.randint(MIN_RANDOM, MAX_RANDOM))
