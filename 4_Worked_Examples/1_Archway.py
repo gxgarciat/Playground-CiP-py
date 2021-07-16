1395fd4b64a4a7995d3df59ba7b38358d6b64642
gxgarciat <info@gxgarciat.dev>
2021-07-16 20:04:08 +0100

    Refactoring

--- 4_Worked_Examples/1_Archway.py
+++ 4_Worked_Examples/1_Archway.py
@@ -1,18 +1,8 @@
 from karel.stanfordkarel import *
 
-"""
-File: Archway.py
-------------------------------
-Karel will move up and over the archway.
-"""
-
-
 def main():
     """
-    You should write your code to make Karel do its task in
-    this function. Make sure to delete the 'pass' line before
-    starting to write your own code. You should also delete this
-    comment and replace it with a better, more descriptive one.
+    Karel will move up and over the archway.
     """
     turn_left()
     movethreetimes()
