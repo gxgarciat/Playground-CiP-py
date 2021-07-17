4137ec2907dcc65fdc1d1517f94327ebc2259e24
gxgarciat <info@gxgarciat.dev>
2021-07-17 20:17:54 +0100

    Refactoring

--- 4_Worked_Examples/1_Obstacles.py
+++ 4_Worked_Examples/1_Obstacles.py
@@ -1,19 +1,14 @@
 from karel.stanfordkarel import *
 
+
+def main():
+
 """
 File: Obstacles.py
 ------------------------------
 Karel will jump over the obstacles and put beepers in the pink squares.
 """
 
-
-def main():
-    """
-    You should write your code to make Karel do its task in
-    this function. Make sure to delete the 'pass' line before
-    starting to write your own code. You should also delete this
-    comment and replace it with a better, more descriptive one.
-    """
     move()
     obstacles()
     obstacles()
