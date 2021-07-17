1d95ddfdaa27057c6ccee4329fbc2197c279d37d
gxgarciat <info@gxgarciat.dev>
2021-07-17 20:16:07 +0100

    Refactoring

--- 4_Worked_Examples/1_Collect_Newspaper.py
+++ 4_Worked_Examples/1_Collect_Newspaper.py
@@ -1,22 +1,19 @@
 from karel.stanfordkarel import *
 
+def main():
+
 """
 File: CollectNewspaperKarel.py
 ------------------------------
 At present, the CollectNewspaperKarel file does nothing.
 Your job in the assignment is to add the necessary code to
+
 instruct Karel to walk to the door of its house, pick up the
 newspaper (represented by a beeper, of course), and then return
 to its initial position in the upper left corner of the house.
 """
 
-def main():
-    """
-    You should write your code to make Karel do its task in
-    this function. Make sure to delete the 'pass' line before
-    starting to write your own code. You should also delete this
-    comment and replace it with a better, more descriptive one.
-    """
+
     rotation()
     move()
     turn_left()
