90e2fbcfa8b1b144943858b1d8b51d7f81884145
gxgarciat <info@gxgarciat.dev>
2021-07-17 20:17:03 +0100

    Refactoring

--- 4_Worked_Examples/1_Move_Beeper.py
+++ 4_Worked_Examples/1_Move_Beeper.py
@@ -1,19 +1,13 @@
 from karel.stanfordkarel import *
 
+def main():
+
 """
 File: MoveBeeper.py
 ------------------------------
 Karel will move the beeper up to the top of its column.
 Karel starts in the bottom left corner, facing East, in front of the beeper, and Karel will end in the top right corner facing East.
 """
-
-def main():
-    """
-    You should write your code to make Karel do its task in
-    this function. Make sure to delete the 'pass' line before
-    starting to write your own code. You should also delete this
-    comment and replace it with a better, more descriptive one.
-    """
 
     move()
     pick_beeper()
