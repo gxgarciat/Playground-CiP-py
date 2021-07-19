185ab2fafc68f1bec9b2c50e4a36ee640a020b8e
gxgarciat <info@gxgarciat.dev>
2021-07-19 21:26:03 +0100

    Refactoring

--- 4_Worked_Examples/2_ConditionalTurn.py
+++ 4_Worked_Examples/2_ConditionalTurn.py
@@ -1,18 +1,13 @@
 from karel.stanfordkarel import *
 
+def main():
+
 """
 File: IfElseWarmup.py
 ------------------------------
 Turn based on whether or not a beeper is present.
 """
 
-def main():
-    """
-    You should write your code to make Karel do its task in
-    this function. Make sure to delete the 'pass' line before
-    starting to write your own code. You should also delete this
-    comment and replace it with a better, more descriptive one.
-    """
     if beepers_present():
         turn_left()
     else:
