29539f935f449653cc1b9cd3d37eaf32d343a862
gxgarciat <info@gxgarciat.dev>
2021-07-20 22:05:39 +0100

    Refactoring

--- 4_Worked_Examples/2_FillBottomRow.py
+++ 4_Worked_Examples/2_FillBottomRow.py
@@ -1,18 +1,14 @@
 from karel.stanfordkarel import *
 
+def main():
+
 """
 File: WhileLoopWarmup.py
 ------------------------------
 Fill the entire bottom row of the world with beepers.
 """
 
-def main():
-    """
-    You should write your code to make Karel do its task in
-    this function. Make sure to delete the 'pass' line before
-    starting to write your own code. You should also delete this
-    comment and replace it with a better, more descriptive one.
-    """
+
     while front_is_clear():
         put_beeper()
         move()
