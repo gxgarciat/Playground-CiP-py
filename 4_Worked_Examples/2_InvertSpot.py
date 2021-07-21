f36a3b7ebd3807dd950e3a830fd8efe12cc3aefe
gxgarciat <info@gxgarciat.dev>
2021-07-21 20:58:10 +0100

    Refactoring

--- 4_Worked_Examples/2_InvertSpot.py
+++ 4_Worked_Examples/2_InvertSpot.py
@@ -1,18 +1,14 @@
 from karel.stanfordkarel import *
 
+
+def main():
+
 """
 File: IfElseWarmup.py
 ------------------------------
 Invert the spot Karel is currently standing on.
 """
 
-def main():
-    """
-    You should write your code to make Karel do its task in
-    this function. Make sure to delete the 'pass' line before
-    starting to write your own code. You should also delete this
-    comment and replace it with a better, more descriptive one.
-    """
     if beepers_present():
         pick_beeper()
     else:
