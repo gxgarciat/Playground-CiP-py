9a3f4ec7493fcf5fea90f41245667d4646ccfbce
gxgarciat <info@gxgarciat.dev>
2021-07-22 21:35:58 +0100

    Refactoring

--- 4_Worked_Examples/2_SpringFlowersM1.py
+++ 4_Worked_Examples/2_SpringFlowersM1.py
@@ -1,19 +1,12 @@
 from karel.stanfordkarel import *
 
+def main():
+
 """
-File: BloomFlower.py
-------------------------------
 Karel will scale the stem of the flower she's facing, bloom the flower with 4 beepers, and return to the ground.
 Karel should end up in the bottommost row, directly to the right of the stem, facing East.
 """
 
-def main():
-    """
-    You should write your code to make Karel do its task in
-    this function. Make sure to delete the 'pass' line before
-    starting to write your own code. You should also delete this
-    comment and replace it with a better, more descriptive one.
-    """
     for i in range(3):
         put_beeper()
         move()
