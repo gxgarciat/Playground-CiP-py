a6cf6ea8b79cb5c256e26167844af19b2cc73776
gxgarciat <info@gxgarciat.dev>
2021-07-22 21:35:16 +0100

    Refactoring

--- 4_Worked_Examples/2_Place_10Beepers.py
+++ 4_Worked_Examples/2_Place_10Beepers.py
@@ -1,18 +1,11 @@
 from karel.stanfordkarel import *
 
-"""
-File: ForLoopWarmup.py
-------------------------------
-Place 10 beepers.
-"""
-
 def main():
-    """
-    You should write your code to make Karel do its task in
-    this function. Make sure to delete the 'pass' line before
-    starting to write your own code. You should also delete this
-    comment and replace it with a better, more descriptive one.
-    """
+ 
+"""
+Place 10 beepers.
+""" 
+ 
     for i in range(10):
         put_beeper()
 
