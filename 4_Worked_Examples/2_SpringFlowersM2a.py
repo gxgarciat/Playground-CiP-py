66bbf03e3bd1dcff6a05ad982c9122556aa520a2
gxgarciat <info@gxgarciat.dev>
2021-07-22 21:36:38 +0100

    Refactoring

--- 4_Worked_Examples/2_SpringFlowersM2a.py
+++ 4_Worked_Examples/2_SpringFlowersM2a.py
@@ -1,18 +1,12 @@
 from karel.stanfordkarel import *
 
+def main():
+
 """
-File: MoveToWall.py
-------------------------------
 Karel will move until front is blocked by a wall.
 """
 
-def main():
-    """
-    You should write your code to make Karel do its task in
-    this function. Make sure to delete the 'pass' line before
-    starting to write your own code. You should also delete this
-    comment and replace it with a better, more descriptive one.
-    """
+
     
     while front_is_blocked():
         turn_left()
