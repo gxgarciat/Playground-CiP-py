dd59fb4f63bcbf835955d32d3576cfea7e14abbe
gxgarciat <info@gxgarciat.dev>
2021-07-23 22:13:19 +0100

    Refactoring

--- 4_Worked_Examples/2_SpringFlowersM2b.py
+++ 4_Worked_Examples/2_SpringFlowersM2b.py
@@ -1,18 +1,11 @@
 from karel.stanfordkarel import *
 
+def main():
+
 """
-File: ClimbStem.py
-------------------------------
 Karel will climb a "stem" -- Karel should start facing a wall. Karel will turn and scale the wall until there is no more wall to Karel's right.
 """
 
-def main():
-    """
-    You should write your code to make Karel do its task in
-    this function. Make sure to delete the 'pass' line before
-    starting to write your own code. You should also delete this
-    comment and replace it with a better, more descriptive one.
-    """
     while front_is_blocked():
         turn_left()
         move()
