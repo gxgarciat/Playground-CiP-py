ca0a55c61215cbcd0f8cd3eba81edd5826f673e6
gxgarciat <info@gxgarciat.dev>
2021-07-24 20:04:54 +0100

    Refactoring

--- 4_Worked_Examples/2_SpringFlowersM4.py
+++ 4_Worked_Examples/2_SpringFlowersM4.py
@@ -1,19 +1,16 @@
 from karel.stanfordkarel import *
 
+def main():
+
+
 """
-File: SpringFlowers.py
-------------------------------
+
 Karel starts in the bottom left corner of a world with 2 empty flower stems, facing East.
 Karel should bloom both flowers with beepers and end in the bottom right corner of the world facing East.
 """
 
-def main():
-    """
-    You should write your code to make Karel do its task in
-    this function. Make sure to delete the 'pass' line before
-    starting to write your own code. You should also delete this
-    comment and replace it with a better, more descriptive one.
-    """
+
+
     while front_is_clear():
         move()
 
