8504132afaedba870aed2162ba10b98f9fdaf903
gxgarciat <info@gxgarciat.dev>
2021-07-25 21:05:49 +0100

    Refactoring

--- 4_Worked_Examples/2_Square.py
+++ 4_Worked_Examples/2_Square.py
@@ -1,19 +1,11 @@
 from karel.stanfordkarel import *
 
+
+def main():
 """
-File: ForLoopWarmup.py
-------------------------------
 Make Karel place beepers in a square (4 beepers total)
 and end in the same position Karel starts in.
 """
-
-def main():
-    """
-    You should write your code to make Karel do its task in
-    this function. Make sure to delete the 'pass' line before
-    starting to write your own code. You should also delete this
-    comment and replace it with a better, more descriptive one.
-    """
 
     put_beeper()
     move()
