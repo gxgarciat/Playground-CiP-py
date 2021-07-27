29ee86496ed260884a5159fea3f250c9d1063284
gxgarciat <info@gxgarciat.dev>
2021-07-27 20:15:02 +0100

    Refactoring

--- 4_Worked_Examples/2_TurnToWall.py
+++ 4_Worked_Examples/2_TurnToWall.py
@@ -1,18 +1,12 @@
 from karel.stanfordkarel import *
 
+def main():
+
 """
-File: WhileLoopWarmup.py
-------------------------------
 Turn left until Karel is facing a wall.
 """
 
-def main():
-    """
-    You should write your code to make Karel do its task in
-    this function. Make sure to delete the 'pass' line before
-    starting to write your own code. You should also delete this
-    comment and replace it with a better, more descriptive one.
-    """
+
     while front_is_clear():
         turn_left()
 
