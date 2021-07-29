e993e1a38af9c5f36936c5ca5a23154f023c7dda
gxgarciat <info@gxgarciat.dev>
2021-07-29 19:20:15 +0100

    Refactoring

--- 4_Worked_Examples/3_AgreementBot.py
+++ 4_Worked_Examples/3_AgreementBot.py
@@ -1,6 +1,10 @@
 def main():
     
+"""
+Write a program that asks the user their favorite animal.
+Then, the program will reply: My favorite animal is also...
 
+"""
     animal = str(input("What's your favorite animal? "))
     print(f'My favorite animal is also {animal}!')
 
