293fd5643f401810f1fcaded64609e376063facf
gxgarciat <info@gxgarciat.dev>
2021-07-31 22:16:23 +0100

    Refactoring

--- 4_Worked_Examples/3_FarenheitToCelsius.py
+++ 4_Worked_Examples/3_FarenheitToCelsius.py
@@ -1,5 +1,11 @@
 def main():
 
+
+"""
+Implement a program to convert F to C
+
+"""
+
     degrees_fahrenheit=float(input("Enter temperature in Fahrenheit: "))
     degrees_celsius = (degrees_fahrenheit - 32) * 5/9
     print(f'Temperature: {degrees_fahrenheit}F = {degrees_celsius}C')
