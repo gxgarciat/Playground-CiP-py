e779bf5cb98608adc01e374b6b6daa21afc5e8cc
gxgarciat <info@gxgarciat.dev>
2021-08-07 16:56:59 +0100

    refactoring

--- 4_Worked_Examples/3_SquareNumber.py
+++ 4_Worked_Examples/3_SquareNumber.py
@@ -1,5 +1,11 @@
 def main():
     
+"""
+Implement a program that provides the user with the squared number
+
+"""    
+    
+    
     number=float(input("Type a number to see its square: "))
     squared = number**2
     print(f'{number} squared is {squared}')
