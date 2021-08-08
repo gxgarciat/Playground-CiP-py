285686fc3033e3c2a4f6543b2ba7f2ce9288411b
gxgarciat <info@gxgarciat.dev>
2021-08-08 10:20:51 +0100

    refactoring

--- 4_Worked_Examples/3_TallEnough.py
+++ 4_Worked_Examples/3_TallEnough.py
@@ -1,5 +1,10 @@
 def main():
 
+"""
+Implement a program that tells the user if they are tall enough to ride
+
+"""      
+
     height=int(input("How tall are you? "))
     if height >= 50:
         print("You're tall enough to ride!")
