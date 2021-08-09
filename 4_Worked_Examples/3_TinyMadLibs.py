601fdc294ceb57788524ab4377e96a71f8c82137
gxgarciat <info@gxgarciat.dev>
2021-08-09 16:48:50 +0100

    Refactoring

--- 4_Worked_Examples/3_TinyMadLibs.py
+++ 4_Worked_Examples/3_TinyMadLibs.py
@@ -1,6 +1,12 @@
 SENTENCE_START = "Code in Place is fun. I learned to program and used Python to make my " # adjective noun verb
 
 def main():
+
+"""
+Implement a program that provides the user with the phrase from CiP
+
+"""    
+    
     adj=str(input("Please type an adjective and press enter. "))
     noun=str(input("Please type an adjective and press enter. "))
     verb=str(input("Please type an adjective and press enter. "))
