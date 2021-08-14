1a7bd66bab6e7ffd87110273f0d3eb36add86f3b
gxgarciat <info@gxgarciat.dev>
2021-08-14 17:22:47 +0100

    Refactoring

--- 1_Assigments/1_Q1_JigsawPuzzle.py
+++ 1_Assigments/1_Q1_JigsawPuzzle.py
@@ -1,6 +1,7 @@
 from karel.stanfordkarel import *
 
 def main():
+
     """
     Write a code that makes Karel to finish the puzzle by picking up the last beeper (puzzle piece) and placing it in the right spot.
     Karel should end in the same position Karel starts in -- the bottom left corner of the world.
