b8fca86f5cbbf16cc55e8450222c0b7f90597da6
gxgarciat <info@gxgarciat.dev>
2021-08-15 19:26:16 +0100

    Refactoring

--- 1_Assigments/1_Q3_CleanupKarel.py
+++ 1_Assigments/1_Q3_CleanupKarel.py
@@ -1,17 +1,12 @@
 from karel.stanfordkarel import *
 
-"""
-File: SafePickup.py
---------------------
-When you finish writing this file, Karel should be able to
-
-"""
-
-def main():
     """
     Write a code to make Karel to pick up a beeper from the current position if one is present
     It should not do anythin if no beepers are present).
     """
+
+def main():
+
 
 # For this repetitive task, a while loop is required.
     while beepers_present():
